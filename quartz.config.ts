9ef9ba81b21e273e38a686eb76c87c4e80f85e88
Merrkry <124278440+merrkry@users.noreply.github.com>
2024-01-08 18:16:45 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "只是旮角而已",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
