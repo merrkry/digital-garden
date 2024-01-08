7421d350e93ab3fcc2dbed710b09b9db432cd23e
Merrkry <124278440+merrkry@users.noreply.github.com>
2024-01-08 21:04:21 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "www.merrkry.top",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
